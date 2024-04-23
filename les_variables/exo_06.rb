e5226517a27da9fdceda5732684174b58dcdf5b4
hassim506 <hassimiou061@gmail.com>
2024-04-23 23:33:14 +0000

    les variables

---
+++ les_variables/exo_06.rb
@@ -0,0 +1,11 @@
+number_of_hours_worked_per_day = 10
+
+number_of_days_worked_per_week = 5
+
+number_of_weeks_in_TS4A = 11
+
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_TS4A}"
+
+
+#On rencontre une erreur avec cette ligne dit au fait des orthographes echanger raison de plus apres compilation il nous demande si did you mean? number_of_weeks_in_TS4A
+puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_TS4A}"
