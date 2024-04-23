ac53d5da5b5f3773f51c5da1529a6128f6ce9a0e
hassim506 <hassimiou061@gmail.com>
2024-04-23 22:34:37 +0000

    Programme qui affiche 3lignes

---
+++ exo_02.rb
@@ -0,0 +1,5 @@
+#Création d'un programme qui affiche Trois lignes!
+
+puts "Bonjour, Monde!"
+puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+puts "En faisant une recherche Google, peux-tu connaître la différence entre print et puts ?"
