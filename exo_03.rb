24726c60609c34af39a437d4bdee968155569e2a
hassim506 <hassimiou061@gmail.com>
2024-04-23 22:43:36 +0000

    Notion de commentaire en ruby

---
+++ exo_03.rb
@@ -0,0 +1,5 @@
+#Ici on remarque qu'en mettant #devant la ligne 2 que lors de l'excution du programme il n'affiche pas la ligne donc # specifie un commentaire en Ruby
+#puts "Bonjour, Monde!"
+
+puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+puts "En faisant une recherche Google, peux-tu connaître la différence entre print et puts ?"
