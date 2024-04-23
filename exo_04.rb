beaee80f511b387d67be835a91838f64e3e377c8
hassim506 <hassimiou061@gmail.com>
2024-04-23 22:51:04 +0000

    Erreur de compilation

---
+++ exo_04.rb
@@ -0,0 +1,8 @@
+#Programme qui ne marche pas
+
+=begin
+ Justification:
+ Ici on se rend compte que lorsque l'on oublie la quote femrante du bout de code le code nous renvoie une erreur
+ "unterminated string meets end of file" qui veut dire qu'il n' ya pas de quote fermante à la fin du fichier string
+=end
+puts "Salut, ça farte ?
