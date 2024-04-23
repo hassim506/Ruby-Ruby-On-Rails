f43fd7ae0aeec4e6149633c1d4102bcc8465f394
hassim506 <hassimiou061@gmail.com>
2024-04-23 23:54:00 +0000

    recuperation de donnees sur la console

---
+++ les_variables/exo_07.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, c'est quoi ton blase ?"
+
+user_name = gets.chomp
+
+puts user_name
+
+#gets.chomps invite l'utilisateur à saisir quelque chose sur la console apres il recuperer ce que l'utilisateur a entrer la stock dans la variable user_name
