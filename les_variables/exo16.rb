c9790d9445336d6611a5789c3f775ee56a9aa70f
hassim506 <hassimiou061@gmail.com>
2024-04-24 22:48:50 +0000

    Virer les années

---
+++ les_variables/exo16.rb
@@ -0,0 +1,4 @@
+#programme exo_16.rb qui va demander son âge à l'utilisateur, et qui, pour chaque année depuis sa naissance, dira "Il y a X ans, tu avais Y ans".
+
+puts "Veuillez saisir votre age"
+age = gets.chomp.to_i
